d0c031b4bab823f85802a8770752af9119eacef7
Eirik W. Talberg <eirik@taldev.no>
2023-11-03 21:11:13 +0100

    test: add integration tests

--- hvat/stdout.py
+++ hvat/stdout.py
@@ -1,7 +1,6 @@
-import json
-
-
 def print_initialization_result(config: dict, result: dict):
-    print("VAULT INIT COMPLETE")
-    print(json.dumps(result, indent=4))
-    print("PLEASE SAVE THIS INFO SOMEWHERE SAFE")
+    print("====VAULT INIT COMPLETE====")
+    print(f"Root token: {result['root_token']}")
+    for key_id, key in result["unseal_keys"].items():
+        print(f"Unseal key {key_id}: {key} ({result['unseal_keys_encoded'][key_id]})")
+    print("====DONE====")
